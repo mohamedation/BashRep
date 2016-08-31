44447fb07b0cf9783519d4ef17fe3c7207bd9060
mohamedation <mohamedation@gmail.com>
2016-09-01 00:40:35 +0300

    Add files via upload
    fedora update script

---
+++ fupdate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+echo "                          "
+echo " _____       _     _       "
+echo "|  |  |___ _| |___| |_ ___ "
+echo "|  |  | . | . | .'|  _| -_|"
+echo "|_____|  _|___|__,|_| |___|"
+echo "      |_|                  "
+echo ""
+dnf clean all
+dnf --refresh upgrade -y
+#dnf clean all
+freshclam
+
+echo "System is updated"
