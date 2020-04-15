df1491bca626a07ca17f7f8735f8f588746ccb51
mohamedation <mohamedation@gmail.com>
2020-04-15 21:48:10 +0200

    Update ubuntu-updateScript.sh

--- ubuntu-updateScript.sh
+++ ubuntu-updateScript.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-# update script for ubuntu/debian based distros. 
+### update script for ubuntu/debian based distros. 
 
 clear
 echo "starting updating process"
-sleep 1
-apt-get update
-apt-get upgrade -y
-apt-get dist-upgrade -y
-apt-get clean
-apt-get autoclean
-apt-get autoremove -y
+apt update
+apt upgrade -y
+apt dist-upgrade -y
+apt clean
+apt autoclean
+apt autoremove -y
 
-#update youtube-dl
-youtube-dl -U
+###update youtube-dl
+#youtube-dl -U
 
-#update Linux Malware Detect
-maldet -u
+###update Linux Malware Detect
+#maldet -u
 
-#stop, update, and start clamav
-/etc/init.d/clamav-freshclam stop
-freshclam
-/etc/init.d/clamav-freshclam start
+###stop, update, and start clamav
+#/etc/init.d/clamav-freshclam stop
+#freshclam
+#/etc/init.d/clamav-freshclam start
 
-#log to a file last update. change to point to the log file.
-echo "Last update:" `date` >> update.log
+###log to a file last update. change to point to the log file.
+#echo "Last update:" `date` >> update.log
+
 echo "system is updated"
 exit
