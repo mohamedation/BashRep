4b363b65745720bafe78a7abf9f739e847a0f32b
mohamedation <mohamedation@gmail.com>
2020-04-15 21:22:10 +0200

    Update and rename instaG.sh to iget.sh

--- iget.sh
+++ iget.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# uses curl and wget to download instagram image from link
+
+
 for var in "$@"
 do
 	image=$(curl $var | grep 'og:image' | sed 's/<meta property="og:image" content="//g'| sed 's/" \/>//g')
