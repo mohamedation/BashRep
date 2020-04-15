863448c70c53127c86825512800b8bfacc618a06
mohamedation <mohamedation@gmail.com>
2020-04-15 21:15:37 +0200

    Update instaG.sh

--- instaG.sh
+++ instaG.sh
@@ -2,6 +2,6 @@
 for var in "$@"
 do
 	image=$(curl $var | grep 'og:image' | sed 's/<meta property="og:image" content="//g'| sed 's/" \/>//g')
-	wget -P /home/mohamedation/Downloads/ $image
+	wget -P /Dir/to/download $image
 done
 
